beae26857c46c32d61c385bc356f2d746190616b
nngobese023 <nngobese023@student.wethinkcode.co.za>
2025-05-21 18:25:54 +0200

    sum diffirence and  product

--- python_introduction/basic_operations.py
+++ python_introduction/basic_operations.py
@@ -1,6 +1,9 @@
-number1 =10
+number1 = 10
 number2 = 5
 
+sum = number1 + number2
+difference = number1 - number2
+product = number1 * number2
 print(f"Addition of {number1} and {number2} is {number1+number2}")
 print(f"Subtraction of {number1} and {number2} is {number1-number2}")
 print(f"Multiplication of {number1} and {number2} is {number1*number2}")
