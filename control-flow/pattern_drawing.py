cbaae3645264318ade37ac1003c5a7fb53935cd4
nngobese023 <nngobese023@student.wethinkcode.co.za>
2025-05-31 21:45:44 +0200

    multiplication table generator

---
+++ control-flow/pattern_drawing.py
@@ -0,0 +1,13 @@
+# pattern_drawing.py
+
+size = int(input("Enter the size of the pattern: "))
+
+row_count = 0
+while row_count < size:
+  
+    for i in range(size): 
+        print("*", end="") 
+    
+    print() 
+
+    row_count += 1
