4c602bc95726b20147865a0cb82026ff2ce72b6c
nngobese023 <nngobese023@student.wethinkcode.co.za>
2025-06-01 13:23:48 +0200

    dailly reminder

--- control-flow/daily_reminder.py
+++ control-flow/daily_reminder.py
@@ -3,7 +3,7 @@ priority = input("Priority (high/medium/low): ")
 time_bound = input("Is it time-bound? (yes/no): ")
 
 match priority :
-    case "hight" :
+    case "high" :
         if time_bound == "yes" :
             print(f"Reminder: '{task}' is a high priority task that requires immediate attention today!")
         elif time_bound == "no" :
